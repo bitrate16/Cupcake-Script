22bd71e33575d94c30b9bb9c46be99a31055e447
Eytlin <dartlevonchic@gmail.com>
2018-07-21 18:03:17 +0300

    Update buildso.sh

--- buildso.sh
+++ buildso.sh
@@ -6,7 +6,7 @@
 
 reset
 
-g++ -static -w -g -c -fPIC -march=x86-64 -std=c++11 -fpermissive $1 -o lib_out.o
+g++ -static -w -g -c -fPIC -std=c++11 -fpermissive $1 -o lib_out.o
 gcc -shared -o $2 lib_out.o -Wl,-y
 #ld -shared lib_out.o -o $2
 rm lib_out.o
